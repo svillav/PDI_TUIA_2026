d2d76000981eea20aa02219f9585ad193085df21
Ornella Bartomioli <ornebar2@gmail.com>
2026-04-07 23:43:24 -0300

    Imagenes

---
+++ PROBLEMA1.py
@@ -0,0 +1,32 @@
+"""
+Problema 1 - Ecualización local de histograma
+"""
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PROBLEMA2.py
@@ -0,0 +1,7 @@
+"""
+Problema 2 - Corrección de múltiple choice
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
